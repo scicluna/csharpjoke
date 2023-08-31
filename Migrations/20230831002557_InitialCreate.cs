d39c25be921837220a1b5bcbb9c481ef9c226805
scicluna <88948700+scicluna@users.noreply.github.com>
2023-08-30 20:31:46 -0400

    initial db setup

---
+++ Migrations/20230831002557_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jokes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JokePrompt = table.Column<string>(type: "TEXT", nullable: true),
+                    JokeAnswer = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jokes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jokes");
+        }
+    }
+}
--- Models/Joke.cs
+++ Models/Joke.cs
@@ -3,8 +3,8 @@ namespace JokeApp.Models
     public class Joke
     {
         public int Id { get; set; }
-        public string JokePrompt { get; set; }
-        public string JokeAnswer { get; set; }
+        public string? JokePrompt { get; set; }
+        public string? JokeAnswer { get; set; }
 
     }
 }
