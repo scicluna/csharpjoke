524e1ae7b6e764b9101d5b459ca41120ec65637a
scicluna <88948700+scicluna@users.noreply.github.com>
2023-08-30 20:17:54 -0400

    tailwind actually works now?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    content: [
+      './**/*.html',
+      './**/*.cshtml',
+    ],
+    options: {
+      safelist: [],  // Specify a safelist of classes that should not be purged
+    },
+  },
   content: [],
   theme: {
     extend: {},
